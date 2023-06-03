3d7d4426e84167f76516edc36aa1c4395eda5119
IVEAU <pressanykey567@gmail.com>
2023-06-03 19:44:32 +0900

    Update GenerateDiraction.cs
    히히

--- Assets/Scripts/GenerateDiraction.cs
+++ Assets/Scripts/GenerateDiraction.cs
@@ -1,8 +1,8 @@
 ﻿public enum GenerateStartPosition
 {
-    Center = 0000,
-    Up = 1000,
-    Right = 0100,
-    Down = 0010,
-    Left = 0001
+    Center,
+    Up,
+    Right,
+    Down,
+    Left
 }
